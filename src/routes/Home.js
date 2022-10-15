4922121e24d5402dfd81921b2a4a2804147087d8
mhlee <mkeasy@gmail.com>
2022-10-16 05:40:22 +0900

    임시저장

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dbService } from 'fbase';
 
 const Home = () => {
   const [tweet, setTweet] = useState('');
+  const [tweets, setTweets] = useState([]);
+  const collectionName = 'clone-tweeter';
+
+  const getTweets = async () => {
+    const datas = await dbService
+      .collection(collectionName)
+      .orderBy('createAt', 'desc')
+      .get();
+    console.log(datas);
+    datas.forEach((document) => {
+      console.log(document.data());
+      // spread attribute
+      const tweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [tweetObject, ...prev]);
+    });
+    console.log(tweets);
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection('clone-tweeter').add({
-      tweet,
+    await dbService.collection(collectionName).add({
+      tweet: tweet,
       createAt: Date.now(),
     });
+    // .then(() => {
+    //   console.log('Document successfully written!');
+    // })
+    // .catch((error) => {
+    //   console.error('Error writing document: ', error);
+    // });
     setTweet('');
   };
 
@@ -33,6 +63,13 @@ const Home = () => {
           />
           <input type="submit" value="Tweet" />
         </form>
+        <div>
+          {/* {tweets.map((doc) => (
+            <div key={doc.id}>
+              <h4>{doc.tweet}</h4>
+            </div>
+          ))} */}
+        </div>
       </div>
     </>
   );

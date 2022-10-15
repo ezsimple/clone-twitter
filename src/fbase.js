b207bcaf68b69b0eaee2d22a5043c962ebceff68
mhlee <mkeasy@gmail.com>
2022-10-16 03:02:12 +0900

    add social login with google and github

--- src/fbase.js
+++ src/fbase.js
@@ -15,4 +15,7 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-export const authService = firebase.auth();
+
+export const firebaseInstance = firebase;
+
+export const authService = firebaseInstance.auth();
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from 'fbase';
+import { authService, firebaseInstance } from 'fbase';
 import React, { useState } from 'react';
 
 const Auth = () => {
@@ -38,6 +38,21 @@ const Auth = () => {
 
   const toggleAccount = () => setNewAccount((prev) => !prev);
 
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === 'google') {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }
+    if (name === 'github') {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+
   return (
     <>
       <form onSubmit={onSubmit}>
@@ -63,8 +78,12 @@ const Auth = () => {
         {newAccount ? 'Sign In' : 'Create Account'}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button name="google" onClick={onSocialClick}>
+          Continue with Google
+        </button>
+        <button name="github" onClick={onSocialClick}>
+          Continue with Github
+        </button>
       </div>
     </>
   );
